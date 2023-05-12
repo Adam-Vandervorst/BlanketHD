43ba589be7857d8354d6f70ef055b1100e2f7a60
Adam-Vandervorst <adam.vandervorst@gmail.com>
2023-05-12 15:35:29 +0200

    Use larger NTE and clean up

--- nte_test.py
+++ nte_test.py
@@ -1,17 +1,9 @@
 from bhv.np import NumPyBoolBHV as BHV
 
-from hedit_utils import HDict
+from hedit_utils import HDict, squash
 
 
-#   R  S
-#   |/
-# A----B
-
-ABRS = HDict({
-    'data': [{'data': "A", 'id': 0}, {'data': "B", 'id': 1}, {'data': "R", 'id': 2}, {'data': "S", 'id': 3}],
-    'conn': [(0, 1), (2, (0, 1)), (3, (0, 1))],
-    'mode': "property_graph"
-})
+NTE = HDict.load_from_path("graphs/InteractiveDisneyStrategy.json", "property_graph")
 
 
 def bias_ps(s, ps, o, pw=1):
@@ -24,29 +16,25 @@ def bias_ps(s, ps, o, pw=1):
 def convert(nte: HDict, pw=1):
     hvs = {n['id']: BHV.rand() for n in nte.find_nodes()}
 
-    adj_ps = {}
-    for src, tgt in nte['conn']:
-        if not isinstance(tgt, tuple) or len(tgt) != 2:
-            continue
-        p = src
-        if tgt in adj_ps:
-            adj_ps[tgt].append(p)
-        else:
-            adj_ps[tgt] = [p]
-
-    for (s, o), ps in adj_ps.items():
+    for s, ps, o in squash(nte.triples(), axis=1):
         hvs[s], hvs[o] = bias_ps(hvs[s], [hvs[p] for p in ps], hvs[o], pw)
 
     return hvs
 
 
-hvs = convert(ABRS, pw=1)
+hvs = convert(NTE, pw=1)
 
-for s, o in ABRS['conn']:
-    if not isinstance(o, int):
-        continue
-
-    ps = [p for (p, e) in ABRS['conn'] if e == (s, o)]
-
-    print(s, o, ps)
-    print([p for p in hvs if p != s and hvs[s].bias_rel(hvs[o], hvs[p]) > .5])
+print("predicting properties")
+for s, ps, o in squash(NTE.triples(), axis=1):
+    print(s, o)
+    print(ps)
+    print([p for p in hvs if p != s and hvs[s].bias_rel(hvs[o], hvs[p]) > .55])
+print()
+print("predicting neighbors")
+for s in NTE['data']:
+    s = s['id']
+    pos = list(squash(NTE.triples(s=s), axis=1))
+    if pos:
+        print(s)
+        print(*pos, sep='\t')
+        print(*[(p, [o for o in os if hvs[s].bias_rel(hvs[o], hvs[p]) > .50]) for (p, os) in pos], sep='\t')
