b000cc7a2656802efe29f169dbe34874f4f1775b
Adam-Vandervorst <adam.vandervorst@gmail.com>
2023-05-13 03:18:18 +0200

    Parallelization for conversion

--- benchmarks/edgebind_blanketbundle.py
+++ benchmarks/edgebind_blanketbundle.py
@@ -5,8 +5,8 @@ from multiprocessing import Pool
 
 
 import networkx as nx
-# from bhv.np import NumPyPacked64BHV as BHV
-from bhv.np import NumPyBoolBHV as BHV
+from bhv.np import NumPyPacked64BHV as BHV
+# from bhv.np import NumPyBoolBHV as BHV
 
 
 def score_undirected_nbs(g: nx.Graph, nbsf, check_frac=.1):
@@ -32,17 +32,10 @@ def score_undirected_nbs(g: nx.Graph, nbsf, check_frac=.1):
 
 def convert(g: nx.Graph, initial=None, ms=10, red=2):
     hvs = initial or {n: BHV.rand() for n in g.nodes}
-    binds = []
 
-    for x, y in g.edges:
-        binds.append(hvs[x] ^ hvs[y])
-
-    E = len(binds)
-    blankets = []
-    for _ in range(ms):
-        ss = sample(binds, int(red*E/ms))
-        blankets.append(BHV.majority(ss))
-
+    with Pool() as p:
+        binds = p.starmap(BHV.__xor__, [(hvs[x], hvs[y]) for x, y in g.edges])
+        blankets = p.map(BHV.majority, [sample(binds, int(red*len(binds)/ms)) for _ in range(ms)])
     return hvs, blankets
 
 
@@ -95,21 +88,21 @@ Workstation 8 cores
 
 100 nodes
 1000 nodes
-1 "blanket"
+1 "blanket" (NOTE this is doing a very large bundle, use NumPyBoolBHV)
 conversion time: 0.41756714361254127 +- 0.0639423438148542
 scoring time: 18.3407090557972 +- 0.3667289361444694
 overshoot loss: 1.4830413311629933 +- 0.01862748160240708
 undershoot loss: 0.9156151054085824 +- 0.0008664566438166838
 
 10 disjoint blankets
-conversion time: 0.27036310725379736 +- 0.08165714798055752
-scoring time: 19.453217881778254 +- 1.5002943874405423
-overshoot loss: 0.8660483205272256 +- 0.004537876946644023
-undershoot loss: 0.8584963204797453 +- 0.0018336164642588188
+conversion time: 5.286508035403676 +- 0.009888001521766079
+scoring time: 0.7203056167811155 +- 0.028054543306225213
+overshoot loss: 0.8763651593649275 +- 0.020374035578414305
+undershoot loss: 0.8555179588353592 +- 0.006161792551931035
 
 1000/15 redundancy 2 blankets (checking 1 in 10)
-conversion time: 0.29750877760816363 +- 0.023177128075548694
-scoring time: 14.277609395817853 +- 0.33073695574466166
-overshoot loss: 0.48151527113324183 +- 0.01243587218646209
-undershoot loss: 0.5596952976672627 +- 0.006146080351486295
+conversion time: 2.72682489764411 +- 0.04742734934242383
+scoring time: 4.615515147196129 +- 0.17808535994692343
+overshoot loss: 0.4832444826882484 +- 0.014321876298373441
+undershoot loss: 0.5522187451599556 +- 0.012364810368286287
 """
