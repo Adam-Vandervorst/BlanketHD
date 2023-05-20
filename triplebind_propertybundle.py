ef557f03e082e926e5f585666db2f62e7bd7659e
Adam-Vandervorst <adam.vandervorst@gmail.com>
2023-05-20 11:48:44 +0200

    Clean up code

--- triplebind_propertybundle.py
+++ triplebind_propertybundle.py
@@ -16,17 +16,15 @@ NTE = HDict.load_from_path("graphs/InteractiveDisneyStrategy.json", "property_gr
 # hvos = {A: {B}, B: {C}, C: {}}
 
 
-def convert(nte: HDict, initial=None, iterations=1, pw1=1, pw2=1):
+def convert(nte: HDict, initial=None):
     ns = [s['id'] for s in nte.find_nodes()]
     hvs = initial or {n: BHV.rand() for n in ns}
-    hvls = initial or {n: [] for n in ns}
-    fhvs = {}
+    hvls = {n: [] for n in ns}
 
     for s, p, o in nte.triples():
         hvls[p].append(hvs[s].permute(-1) ^ hvs[o].permute(1))
 
-    for n in ns:
-        fhvs[n] = BHV.majority(hvls[n])
+    fhvs = {n: BHV.majority(hvls[n]) for n in ns}
 
     return hvs, fhvs
 
@@ -47,21 +45,3 @@ for s in NTE['data']:
         print(*pos, sep='\t')
         print(*[(p, [o for o in os if not (hvs[s].permute(-1) ^ fhvs[p]).unrelated(hvs[o].permute(1), 3)]) for (p, os) in pos], sep='\t')
 
-# print()
-# print("predicting properties (s, ?, -)")
-# for s in NTE['data']:
-#     s = s['id']
-#     ps = list(set(p for p, _ in NTE.triples(s=s)))
-#     if ps:
-#         print(s)
-#         print(ps)
-#         print([p for p in hvps if p != s and not (hvps[s]).unrelated(hvos[p], 3)])
-# print()
-# print("predicting neighbors (s, -, ?)")
-# for s in NTE['data']:
-#     s = s['id']
-#     os = list(set(o for _, o in NTE.triples(s=s)))
-#     if os:
-#         print(s)
-#         print(os)
-#         print([o for o in hvos if o != s and not (hvos[s]).unrelated(hvps[o], 3)])
